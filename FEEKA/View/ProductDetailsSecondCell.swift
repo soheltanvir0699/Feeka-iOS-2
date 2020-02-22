11913838f0abeff38eebacddcaf4357897756fb0
Sohel Tanvir <soheltanvir0699@gmail.com>
2020-02-23 01:15:13 +0600

    single product dtails update

---
+++ FEEKA/View/ProductDetailsSecondCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductDetailsSecondCell.swift
+//  FEEKA
+//
+//  Created by Apple Guru on 22/2/20.
+//  Copyright © 2020 Apple Guru. All rights reserved.
+//
+
+import UIKit
+
+class ProductDetailsSecondCell: UICollectionViewCell {
+    
+}
