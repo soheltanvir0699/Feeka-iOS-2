6e6c2953272e7f67fe5f09c2540fe747ebee4bd0
Sohel Tanvir <soheltanvir0699@gmail.com>
2020-02-24 01:00:02 +0600

    update add address and remove and edit address

--- FEEKA/Controller/AddAddressDetailsViewController.swift
+++ FEEKA/Controller/AddAddressDetailsViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 import CoreLocation
 import MapKit
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
 
 class AddAddressDetailsViewController: UIViewController, CLLocationManagerDelegate ,MKMapViewDelegate{
     @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var defaultCheckBox: UIButton!
     @IBOutlet weak var navView: UIView!
+    @IBOutlet weak var nameLbl: UITextField!
+    @IBOutlet weak var sureNameLbl: UITextField!
+    @IBOutlet weak var contactNumberLbl: UITextField!
+    @IBOutlet weak var companyLbl: UITextField!
+    @IBOutlet weak var streetAddressLbl: UITextField!
+    @IBOutlet weak var suburbLbl: UITextField!
+    @IBOutlet weak var cityLbl: UITextField!
+    @IBOutlet weak var postcodeLbl: UITextField!
     
+    var name = ""
+    var sureName = ""
+    var phone = ""
+    var country = ""
+    var streetAdd = ""
+    var suburb = ""
+    var city = ""
+    var postalCode = ""
     var isDefault = true
+    var company = ""
+    var indicator:NVActivityIndicatorView!
+    let userdefault = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         navView.setShadow()
+        setUPView()
+    }
+    
+    func setUPView() {
+        nameLbl.text = name
+        sureNameLbl.text = sureName
+        contactNumberLbl.text = phone
+        streetAddressLbl.text = streetAdd
+        suburbLbl.text = suburb
+        cityLbl.text = city
+        postcodeLbl.text = postalCode
+        companyLbl.text = company
     }
     
     @IBAction func backBtn(_ sender: Any) {
@@ -40,6 +74,67 @@ class AddAddressDetailsViewController: UIViewController, CLLocationManagerDelega
        }
     
     @IBAction func saveBtn(_ sender: Any) {
+        var customerId = ""
+        indicator = self.indicator()
+        guard let url = URL(string: "https://feeka.co.za/json-api/route/add_address.php") else {
+                           self.view.makeToast( "Please try again later")
+                              return
+                          }
+        if userdefault.value(forKey: "customer_id") != nil {
+            customerId = userdefault.value(forKey: "customer_id") as! String
+        }
+        
+        if nameLbl.text == "" || suburbLbl.text == "" || sureNameLbl.text == "" || contactNumberLbl.text == "" || streetAddressLbl.text == "" || cityLbl.text == "" || postcodeLbl.text == "" || companyLbl.text == "" {
+            self.view.makeToast("Please Enter Correct Value")
+            return
+        } else if (contactNumberLbl.text)?.count != 10 {
+            self.view.makeToast("Phone Number Is Invalid")
+            return
+        } else {
+                          
+                              let paramater = [
+                                 "address_id": "",
+                                 "customer_id": "\(customerId)",
+                                "Name": "\(nameLbl.text!)",
+                                "Surname": "\(sureNameLbl.text!)",
+                                 "Unit_Number": "",
+                                 "Apartment": "",
+                                 "Company": "\(companyLbl.text!)",
+                                "Street_Address": "\(streetAddressLbl.text!)",
+                                "Suburb": "\(suburbLbl.text!)",
+                                 "Country": "South Africa",
+                                 "Postal_Code": "\(postcodeLbl.text!)",
+                                "City": "\(cityLbl.text!)",
+                                "Contact_Number": "\(contactNumberLbl.text!)",
+                                 "is_default":"0"
+                                 ]
+        
+                              Alamofire.request(url, method: .post, parameters: paramater, encoding: JSONEncoding.default, headers: nil).response { (response) in
+                                  self.indicator.startAnimating()
+                                  if let error = response.error {
+                                      self.indicator.stopAnimating()
+                                      let alertView = ShowAlertView().alertView(title: "Something went wrong", action: "OK", message: "Please try again.")
+                                      self.present(alertView, animated: true, completion: nil)
+                                      print(error)
+                                      
+                                  }
+                                  
+                                  if let result = response.data {
+                                      let jsonRespose = JSON(result)
+                                     
+                                      if jsonRespose["status"].stringValue == "1" {
+                                        self.navigationController?.popViewController(animated: true)
+                                      } else {
+                                        
+                                        self.view.makeToast("Something Wrong")
+                                        
+                                      }
+
+                                      self.indicator.stopAnimating()
+                                  }
+                              
+                  }
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
--- FEEKA/Controller/AddAddressViewController.swift
+++ FEEKA/Controller/AddAddressViewController.swift
@@ -7,20 +7,114 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
 class AddAddressViewController: UIViewController {
 
     @IBOutlet weak var navView: UIView!
+    @IBOutlet weak var defaultAddress: UILabel!
+    @IBOutlet weak var addressView: UIView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var country: UILabel!
+    @IBOutlet weak var homeAdress: UILabel!
+    @IBOutlet weak var suburb: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var postalCode: UILabel!
+    @IBOutlet weak var phoneNumber: UILabel!
+    @IBOutlet weak var editAddress: UIImageView!
+    
+    @IBOutlet weak var deleteAddress: UIImageView!
+    var indicator: NVActivityIndicatorView!
+    var name1 = ""
+    var sureName1 = ""
+    var phone1 = ""
+    var country1 = ""
+    var streetAdd1 = ""
+    var suburb1 = ""
+    var city1 = ""
+    var postalCode1 = ""
+    var company = ""
+    let userdefault = UserDefaults.standard
+    var customerId = ""
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        alertView()
+        editAddress.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(editProfile)))
+        deleteAddress.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(deleteProfile)))
         navView.setShadow()
+        addressView.setShadow()
+        
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        if userdefault.value(forKey: "customer_id") != nil {
+            customerId = userdefault.value(forKey: "customer_id") as! String
+        }
+        getAddressApi()
+    }
     @IBAction func backBtn(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
     
+    @objc func editProfile() {
+        let addressDetails = storyboard?.instantiateViewController(withIdentifier: "AddAddressDetailsViewController") as? AddAddressDetailsViewController
+        addressDetails!.name = name1
+        addressDetails?.sureName = sureName1
+        addressDetails?.phone = phone1
+        addressDetails?.country = country1
+        addressDetails?.streetAdd = streetAdd1
+        addressDetails?.suburb = suburb1
+        addressDetails?.city = city1
+        addressDetails?.postalCode = postalCode1
+        addressDetails?.company = company
+        self.navigationController?.pushViewController(addressDetails!, animated: true)
+    }
+    
+    @objc func deleteProfile() {
+        indicator = self.indicator()
+        let address_id = userdefault.value(forKey: "address_id") as! String
+        guard let url = URL(string: "https://feeka.co.za/json-api/route/remove_address.php") else {
+                           self.view.makeToast( "Please try again later")
+                              return
+                          }
+                          
+                              let paramater = [
+                                 
+                                 "Address_id": "\(address_id)",
+                                 "customer_id": "\(customerId)",
+                                 "is_default":"1"
+                                 
+                              ]
+                              Alamofire.request(url, method: .post, parameters: paramater, encoding: JSONEncoding.default, headers: nil).response { (response) in
+                                  self.indicator.startAnimating()
+                                  if let error = response.error {
+                                      self.indicator.stopAnimating()
+                                      let alertView = ShowAlertView().alertView(title: "Something went wrong", action: "OK", message: "Please try again.")
+                                      self.present(alertView, animated: true, completion: nil)
+                                      print(error)
+                                      
+                                  }
+                                  
+                                  if let result = response.data {
+                                      let jsonRespose = JSON(result)
+                                     
+                                      if jsonRespose["status"].stringValue == "1" {
+                                        self.addressView.isHidden = true
+                                        self.defaultAddress.isHidden = true
+                                      } else {
+                                        
+                                        self.addressView.isHidden = false
+                                        self.defaultAddress.isHidden = false
+                                        
+                                      }
+
+                                      self.indicator.stopAnimating()
+                                  }
+                              
+                  }
+    }
+    
+ 
     @IBAction func addAddressBtn(_ sender: Any) {
         let addressDetails = storyboard?.instantiateViewController(withIdentifier: "AddAddressDetailsViewController") as? AddAddressDetailsViewController
         self.navigationController?.pushViewController(addressDetails!, animated: true)
@@ -30,6 +124,71 @@ class AddAddressViewController: UIViewController {
         let alertVc = ShowAlertView().alertView(title: "Please add new address", action: "OK", message: "")
            self.present(alertVc, animated: true, completion: nil)
          }
+    
+    fileprivate func updateView(_ name: String, _ sureName: String, _ phone: String, _ country: String, _ streetAdd: String, _ suburb: String, _ city: String, _ postalCode: String) {
+        self.name.text = "\(name1) \(sureName1)"
+        self.phoneNumber.text = phone1
+        self.country.text = country1
+        self.homeAdress.text = streetAdd1
+        self.suburb.text = suburb1
+        self.city.text = city1
+        self.postalCode.text = postalCode1
+        
+        self.addressView.isHidden = false
+        self.defaultAddress.isHidden = false
+    }
+    
+    func getAddressApi() {
+        indicator = self.indicator()
+        guard let url = URL(string: "https://feeka.co.za/json-api/route/get_address.php") else {
+                           self.view.makeToast( "Please try again later")
+                              return
+                          }
+                          
+                              let paramater = [
+                                 "customer_id":"\(customerId)"
+                              ]
+                              Alamofire.request(url, method: .post, parameters: paramater, encoding: JSONEncoding.default, headers: nil).response { (response) in
+                                  self.indicator.startAnimating()
+                                  if let error = response.error {
+                                      self.indicator.stopAnimating()
+                                      let alertView = ShowAlertView().alertView(title: "Something went wrong", action: "OK", message: "Please try again.")
+                                      self.present(alertView, animated: true, completion: nil)
+                                      print(error)
+                                      
+                                  }
+                                  
+                                  if let result = response.data {
+                                      let jsonRespose = JSON(result)
+                                     
+                                      if jsonRespose["status"].stringValue == "1" {
+                                       let data = jsonRespose["data"].arrayValue[0]
+                                        self.name1 = data["Name"].stringValue
+                                        self.sureName1 = data["Surname"].stringValue
+                                        self.phone1 = data["Contact_Number"].stringValue
+                                        self.country1 = data["Country"].stringValue
+                                        self.streetAdd1 = data["Street_Address"].stringValue
+                                        self.suburb1 = data["Suburb"].stringValue
+                                        self.city1 = data["City"].stringValue
+                                        self.postalCode1 = data["Postal_Code"].stringValue
+                                        self.company = data["Company"].stringValue
+                                        let addressId = data["address_id"].stringValue
+                                        self.userdefault.setValue(addressId, forKey: "address_id")
+                                        self.updateView(self.name1, self.sureName1, self.phone1, self.country1, self.streetAdd1, self.suburb1, self.city1, self.postalCode1)
+                                       
+                                      } else {
+                                        
+                                        self.alertView()
+                                        self.addressView.isHidden = true
+                                        self.defaultAddress.isHidden = true
+                                        
+                                      }
+
+                                      self.indicator.stopAnimating()
+                                  }
+                              
+                  }
+    }
          
 
 }
